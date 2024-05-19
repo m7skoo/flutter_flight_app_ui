447ed579768de877c667814328d034fc7607d5b9
m7skoo <m7skoo.88@gmail.com>
2024-05-19 11:31:17 +0300

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -16,12 +16,11 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-          useMaterial3: true,
-          primaryColor: const Color(0xff415a5c),
-          indicatorColor: const Color(0xffffcfa1),
-          canvasColor: const Color(0xff9dafb1)
-          //canvasColor: const Color(0xff597672),
-          ),
+        useMaterial3: true,
+        primaryColor: const Color(0xff415a5c),
+        indicatorColor: const Color(0xffffcfa1),
+        canvasColor: const Color(0xff9dafb1),
+      ),
       home: const SplashScreen(),
     );
   }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,17 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cached_network_image/cached_network_image.dart'; // Import cached network image package
 import 'package:flight_app_ui/screens/detail_screen.dart';
 import 'package:flight_app_ui/widgets/animated_route.dart';
 import 'package:flight_app_ui/widgets/show_up_animation.dart';
 import 'package:flight_app_ui/widgets/text.dart';
-import 'package:flutter/material.dart';
 import '../data/flight_data.dart';
 import '../widgets/flight_card.dart';
 import 'flightBooking/add_flight.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  const HomeScreen({Key? key});
 
   @override
   Widget build(BuildContext context) {
+    FirebaseAuth auth = FirebaseAuth.instance;
+    User? currentUser = auth.currentUser;
+
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
@@ -26,32 +31,107 @@ class HomeScreen extends StatelessWidget {
               overflow: TextOverflow.ellipsis,
             ),
             TextUtil(
-              text: "Dev_73arner",
+              text: currentUser?.displayName ?? "Guest",
               color: Theme.of(context).primaryColor,
               overflow: TextOverflow.ellipsis,
             ),
           ],
         ),
         elevation: 0,
-        leading: Icon(
-          Icons.account_tree,
-          color: Theme.of(context).primaryColor,
+        leading: Builder(
+          builder: (context) => IconButton(
+            icon: Icon(
+              Icons.account_tree,
+              color: Theme.of(context).primaryColor,
+            ),
+            onPressed: () {
+              Scaffold.of(context).openDrawer();
+            },
+          ),
         ),
-        actions: [
-          Container(
-            height: 50,
-            width: 50,
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                image: const DecorationImage(
-                  image: AssetImage("assets/profile.png"),
-                )),
-          ),
-          const SizedBox(
-            width: 20,
-          ),
-        ],
         backgroundColor: Colors.white,
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Theme.of(context).primaryColor,
+              ),
+              child: ListView(
+                padding: EdgeInsets.zero,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      // Display user profile image
+                      SizedBox(
+                        width: 100,
+                        height: 100,
+                        child: currentUser?.photoURL != null
+                            ? CachedNetworkImage(
+                                imageUrl: currentUser!.photoURL!,
+                                placeholder: (context, url) =>
+                                    const CircularProgressIndicator(),
+                                errorWidget: (context, url, error) =>
+                                    const Icon(Icons.error),
+                                fit: BoxFit.cover,
+                              )
+                            : const Placeholder(), // Placeholder if user doesn't have a profile picture
+                      ),
+                      const SizedBox(height: 10),
+                      Text(
+                        currentUser?.displayName ??
+                            '', // Display user's display name
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 24,
+                        ),
+                      ),
+                      const SizedBox(height: 5),
+                      Text(
+                        currentUser?.email ?? '', // Display user's email
+                        style: const TextStyle(
+                          color: Colors.black,
+                          fontSize: 14,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            ListTile(
+              leading: const Icon(Icons.account_circle),
+              title: const Text('Profile'),
+              onTap: () {
+                // Handle profile navigation
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.settings),
+              title: const Text('Settings'),
+              onTap: () {
+                // Handle settings navigation
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.airplanemode_inactive_sharp),
+              title: const Text('Airline'),
+              onTap: () {
+                // Handle airline navigation
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.star),
+              title: const Text('Rate the App'),
+              onTap: () {
+                // Handle app rating navigation
+              },
+            ),
+          ],
+        ),
       ),
       body: Column(
         children: [
@@ -68,36 +148,42 @@ class HomeScreen extends StatelessWidget {
             ),
           ),
           Expanded(
-              child: Container(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            decoration: BoxDecoration(
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              decoration: BoxDecoration(
                 color: Theme.of(context).primaryColor,
                 borderRadius:
-                    const BorderRadius.vertical(top: Radius.circular(30))),
-            alignment: Alignment.topCenter,
-            child: Padding(
-              padding: const EdgeInsets.only(top: 30),
-              child: ListView.builder(
+                    const BorderRadius.vertical(top: Radius.circular(30)),
+              ),
+              alignment: Alignment.topCenter,
+              child: Padding(
+                padding: const EdgeInsets.only(top: 30),
+                child: ListView.builder(
                   itemCount: flightList.length,
                   shrinkWrap: true,
                   itemBuilder: (context, index) {
                     return ShowUpAnimation(
-                        delay: 150 * index,
-                        child: GestureDetector(
-                            onTap: () {
-                              Navigator.of(context).push(MaterialPageRoute(
-                                  builder: (context) => DetailScreen(
-                                        data: flightList[index],
-                                        index: index,
-                                      )));
-                            },
-                            child: FlightCard(
+                      delay: 150 * index,
+                      child: GestureDetector(
+                        onTap: () {
+                          Navigator.of(context).push(MaterialPageRoute(
+                            builder: (context) => DetailScreen(
                               data: flightList[index],
                               index: index,
-                            )));
-                  }),
+                            ),
+                          ));
+                        },
+                        child: FlightCard(
+                          data: flightList[index],
+                          index: index,
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
             ),
-          ))
+          ),
         ],
       ),
       floatingActionButton: FloatingActionButton(
@@ -106,7 +192,6 @@ class HomeScreen extends StatelessWidget {
           Navigator.of(context).push(MyCustomAnimatedRoute(
             enterWidget: const AddFlightScreen(),
           ));
-          // Navigator.of(context).push(MaterialPageRoute(builder: (context)=>AddFlightScreen()));
         },
         child: Icon(
           Icons.add,
